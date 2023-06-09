3f55bfe5784cd0185395838dd9791321c2fef3cd
Samuel Lawal <samuellawal60@gmail.com>
2023-06-09 02:56:25 +0100

    chore: Added more content to landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {BsArrowDownCircle} from "react-icons/bs"
 
 import MovieCard from "../components/organisms/MovieCard";
 
@@ -8,13 +8,21 @@ import MovieCard from "../components/organisms/MovieCard";
 const Home = () => {
 
   return (
-    <main className="home_container">
-  <div className="flex justify-center flex-col justify-center items-center h-96 pt-12">
+      <> 
+    <div className="home_container">
+  <section className="flex justify-center flex-col justify-center items-center h-4/6 pt-12">
     <p className="text-small">introducing</p>
     <h1 className="text-white text-center leading-none  text-4xl md:text-7xl font-extrabold">Movie<span className="text-brand-red">Time</span><br/>Spotlight Picks.</h1>
     <p className="text-sm md:text-sm text-center pt-4 md:pt-6">Immerse Yourself in the Exciting World of Trending Movies and TV Series with MovieTime <br/>cause why not enjoy soft life.</p>
+      </section>
+      <div className="flex items-center justify-center">
+      <BsArrowDownCircle className="text-center" size='2em'/>
       </div>
-    </main>
+    </div>
+    <section className="container mx-auto">
+     
+    </section>
+    </>
   );
 };
 
