1e185d171c3bb3df40205a4e325c846ba1f70ad9
Samuel Lawal <samuellawal60@gmail.com>
2023-06-09 02:14:41 +0100

    fix: adjust font-size for landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   <div className="flex justify-center flex-col justify-center items-center h-96 pt-12">
     <p className="text-small">introducing</p>
     <h1 className="text-white text-center leading-none  text-4xl md:text-7xl font-extrabold">Movie<span className="text-brand-red">Time</span><br/>Spotlight Picks.</h1>
-    <p className="text-small text-center pt-4 md:pt-6">Immerse Yourself in the Exciting World of Trending Movies and TV Series with MovieTime <br/>cause why not enjoy soft life.</p>
+    <p className="text-sm md:text-sm text-center pt-4 md:pt-6">Immerse Yourself in the Exciting World of Trending Movies and TV Series with MovieTime <br/>cause why not enjoy soft life.</p>
       </div>
     </main>
   );
