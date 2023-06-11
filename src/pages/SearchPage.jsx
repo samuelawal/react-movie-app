05d5fa1c9f69e07bbedcd3133a63ba5e4ea02a1d
Samuel Lawal <samuellawal60@gmail.com>
2023-06-12 00:31:53 +0100

    feat: Route search page to app

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,26 +1,39 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useOutletContext } from 'react-router-dom'
 import MovieCard from '../components/organisms/MovieCard'
 
+// FIXME: Please find a better way of doing this
+
 const SearchPage = ({handleSearchResults, searchResults, isSearchingResult}) => {
   const [query] = useOutletContext()
+  const [mobileSearchValue, setMobileSearchValue] = useState()
+  const handleValueChange = (event) => {
+    setMobileSearchValue(event.target.value);
+  };
   useEffect(() => {
     const searchQuery = async () => {
+      if(query) {
         await handleSearchResults(query)
+      }
+      if(mobileSearchValue) {
+        await handleSearchResults(mobileSearchValue)
+      }
     }
     searchQuery()
-  }, [query])
+  }, [mobileSearchValue, query])
   return (
     <div>
+      <div className='mx-6 md:hidden block'>
+      <input type='text' value={mobileSearchValue} onChange={handleValueChange} className=' w-full d-block text-sm search_input px-3 rounded py-2' placeholder='Search for movies, series...'/>
+      </div>
      {
       isSearchingResult ? (
         "Loading..."
       ) : (
-        <div className='container mx-3 md:mx-auto'>
-          
+        <div className='container mx-3 md:mx-auto mt-4 md:mt-0'>  
           <h2 className='text-xl'>Search results found :</h2>
           {!searchResults.length ? (
-            <p className='text-center text-red-500 m-5'>No result found!</p>
+            <p className='text-center  m-5'>No result found!</p>
           ) : <MovieCard movies={searchResults}/>}         
         </div>
       )
